f2fde2c605c3134fe19f7a822b303457137ac489
Joao-hey <joaoribcorreia@gmail.com>
2022-12-09 14:03:48 +0100

    added all needed files except js

---
+++ view/Content/waitScreen.php
@@ -0,0 +1,9 @@
+<link rel="stylesheet" href="../View/Styles/waitScreenStyles.css">
+<a href="">
+    <div>
+    <h1>adiawpidpm</h1>
+    </div>
+    <div>
+
+    </div>
+</a>
--- view/PageBuilder.php
+++ view/PageBuilder.php
@@ -1,1 +1,37 @@
-function page
+<?php
+
+class Pagebuilder
+{
+
+    public static function pagebuild($title = "", $url = "")
+    {
+    echo"
+    <!DOCTYPE html>
+    <html lang=\"en\">
+    <head>
+        <meta charset=\"UTF-8\">
+        <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">
+        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
+        <title>" . $title . "</title>
+        <link rel=\"stylesheet\" href=\"../View/Styles/basicStyles.css\">
+    </head>
+    <body>
+        
+        <div class=\"main\">
+        
+        ";
+        
+        require_once "Content/" . $url . ".php";
+        
+        echo
+        "
+        </div>
+    <footer>
+        
+    </footer>
+    </body>
+    
+    </html>
+    ";
+    }
+}
